385404d6cf02366f5f02ed07820ecdcb8044fd72
Zach Antosko <zachary.antosko@gmail.com>
2021-05-20 08:23:41 -0500

    initial commit

---
+++ pokemon-cards/src/components/Card.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import "./cardContainer.css"
+
+class Card extends Component {
+	state = {
+		flip: false
+	}
+	render() {
+		const {  name, sprites, stats, hp, types, element } = this.props.pokemon
+    const newStats = [...stats]
+    const mappedStats = newStats.map((stat,idx) => <span key={idx}>{stat.base_stat}</span>)
+
+    //* Changes card color based on type
+    // const poke_types = types.map(type => type.type.name);
+    // const type = mainTypes.find(type => poke_types.indexOf(type) > -1);
+    // const color = colors[type];
+
+    const { flip } = this.state
+		const uppercased = name.toUpperCase()
+		return (
+			<div className="pokemon-card" style={{backgroundColor: `${colors.fire}`}}>
+				<img onMouseLeave={()=> this.setState({flip: !flip})} onMouseOver={()=> this.setState({flip: !flip})} src={flip ? sprites.back_shiny : sprites.front_shiny } alt="" />
+				<h2>{uppercased}</h2>
+				<p>HP: {mappedStats[0] ? mappedStats[0] : hp}</p>
+        {/* <p>Type: {type ? type : element}</p> */}
+			</div>
+		);
+	}
+}
+
+//* Color canvas
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5'
+};
+const mainTypes = Object.keys(colors)
+
+
+export default Card;
---
+++ pokemon-cards/src/components/CardContainer.jsx
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import Card from "./Card"
+import "./cardContainer.css"
+
+class CardContainer extends Component {
+  //* Setting state
+  state = {
+    searchCriteria: "",
+    pokemonList: [],
+    pokemonStat: "",
+    pokedexNum: "",
+    pokemonBerries: []
+  }
+
+  //* Allows us to add props to the state of the CardContainer component. This allows us to make updates.
+  componentDidMount() {
+  //? May use this for reference later
+  //   const berryUrl = "https://pokeapi.co/api/v2/berry/"
+  //   const fetchData = await fetch(berryUrl,{ headers: {
+  //   'Content-Type': 'application/json'
+    
+  // },})
+  //   const json = await fetchData.json()
+  //   this.setState({
+  //     pokemonBerries:json.results
+  //   })
+    
+    //! Easy Mode
+    //* Sets state of pokemonList to to the data that was passed down from the App component.
+    // this.setState({
+    //   pokemonList: this.props.pokemonData[0].pokemon
+    // })
+
+    //! Hard Mode
+    const pokemonArray = []
+    const pokemonURL = `https://pokeapi.co/api/v2/pokemon?limit=151`
+    fetch(pokemonURL)
+      .then(res => res.json())
+      .then((allpokemon) => {
+        allpokemon.results.forEach(pokemon => fetchPokemonData(pokemon))
+      })
+    
+      function fetchPokemonData(pokemon) {
+        let url = pokemon.url
+        fetch(url)
+          .then(res => res.json())
+          .then(pokeData => pokemonArray.push(pokeData))
+      }
+
+    console.log(pokemonArray)
+    this.setState({
+      pokemonList: pokemonArray
+    })
+  }
+
+  //* This method grabs the input value from the search bar and sets the state of searchCriteria to the value that was grabbed.
+  searchCriteria = (e) => {
+    const search = e.target.value.toLowerCase()
+    
+    this.setState({
+      searchCriteria: search
+    })
+  }
+  //* This method is used for handling multiple user inputs. It sets the state of the changed input to the value of that same input. This makes it a "controlled" input.
+  sendPokemonToData = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    e.preventDefault();
+  }
+
+
+  //* This method handles form submission.
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const pokemonID = this.state.pokedexNum
+    const pokemonFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+    const specificPokemon = await pokemonFetch.json()
+
+    //* Setting all the submitted input values as the key values in a newPokemon object.
+    const newPokemon = {
+      name: specificPokemon.name,
+      stats: this.state.pokemonStat,
+      hp: specificPokemon.stats[0].base_stat,
+      element: specificPokemon.types[0].type.name,
+      sprites: {
+        front_shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemonID}.png`,
+        back_shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${pokemonID}.png`
+      }
+    }
+
+    //* Creates a new array and assigns it to newPokemonList. Prepends the newPokemon object to the new array. Then "spreads" the old array that was passed down into the new array.
+    const newPokemonList = [newPokemon, ...this.state.pokemonList]
+
+    //* Sets state of pokemonList to the newly created array.
+    this.setState({
+      pokemonList: newPokemonList
+    })
+  }
+
+
+  render() {
+      
+    const {searchCriteria, pokemonList} = this.state
+    
+    const filteredData = pokemonList.filter(pokemon => pokemon.name.includes(searchCriteria)) ? pokemonList.filter(pokemon => pokemon.name.includes(searchCriteria)) : pokemonList
+
+    return (
+      <div className="main-card-container">
+        <div>
+        <input className="search-field" onChange={(e)=>this.searchCriteria(e)} type="text" placeholder="Search for a pokemon"/>
+
+        </div>
+
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          {/* <input 
+            name="pokemonName"
+            className="create-field"
+            type="text" 
+            placeholder="Enter a Name"
+            value={this.state.pokemonName}
+            onChange={(e) => this.sendPokemonToData(e)}
+          />
+          <input 
+            name="pokemonHP"
+            className="create-field" 
+            type="number" 
+            placeholder="Enter a HP"
+            value={this.state.pokemonHP}
+            onChange={(e) => this.sendPokemonToData(e)}
+          /> */}
+          <input
+            name="pokedexNum"
+            className="create-field" 
+            type="text" 
+            placeholder="Enter Pokedex Number"
+            value={this.state.pokedexNum}
+            onChange={(e) => this.sendPokemonToData(e)}
+          />
+          <br/>
+          <input className="create-field" type="submit" value="submit"/>
+        </form>
+        <div className="card-container">
+          {filteredData.map((singlePokemon) => <Card key={singlePokemon.name}  pokemon={singlePokemon}  />
+          )}
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+
+export default CardContainer;
